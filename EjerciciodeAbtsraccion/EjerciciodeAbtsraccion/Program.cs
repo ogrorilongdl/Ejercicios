d13c6a01a7ace5280f8c4445057905c9362d0af2
Christopher POnce <chrisclassdev@gmail.com>
2025-08-22 20:55:57 -0600

    juntarlaram1

--- EjerciciodeAbtsraccion/EjerciciodeAbtsraccion/Program.cs
+++ EjerciciodeAbtsraccion/EjerciciodeAbtsraccion/Program.cs
@@ -12,6 +12,8 @@ namespace EjerciciodeAbtsraccion
         {
             Console.WriteLine("Hola Git Hub");
             Console.WriteLine("Me he cambiado a la rama de develop");
+            Console.WriteLine("juntos la rama 1");
+        
         }
     }
 }
