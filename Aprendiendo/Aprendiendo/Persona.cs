c5f3f1eab84611ec453d67e28a13bf83c5048a7e
Christopher POnce <chrisclassdev@gmail.com>
2025-10-07 23:07:53 -0600

    Reapply "Merge pull request #5 from ogrorilongdl/AprendiendoCommit"
    This reverts commit c33415abd39c84c808af41769deadd5f69dffbf9.

---
+++ Aprendiendo/Aprendiendo/Persona.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aprendiendo
+{
+    public class Persona
+    {
+
+        public int edad { get; set; }
+        public string nombre { get; set; }
+
+        public Persona()
+        {
+                edad = 12;
+                nombre = "Carlos";
+        }
+
+        public static  void Saludar()
+        {
+
+            Console.WriteLine("Buenos dias");
+
+
+        }
+
+        public void Ejecutar () { 
+        
+        Console.WriteLine($" tu nombre es {nombre} y tu edad {edad}");
+        
+        
+        }
+
+
+
+
+    }
+}
